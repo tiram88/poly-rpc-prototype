2780b653b14c5becdc929f552a9e7888415536e5
CEPEC\georges.kuenzli <georges.kuenzli@cepec.com>
2022-09-30 19:30:45 +0200

    Switch to bidirectional stream RPC

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use poly_rpc_prototype::protowire::KaspadResponse;
 use poly_rpc_prototype::protowire::{
     rpc_client::RpcClient,
     KaspadRequest, kaspad_request,
@@ -11,22 +12,38 @@ use poly_rpc_prototype::protowire::{
 // use rand::rngs::ThreadRng;
 // use rand::Rng;
 // use tokio::time;
-// use tonic::transport::Channel;
+use tonic::transport::Channel;
 use tonic::Request;
+
+async fn run_get_block(
+    client: &mut RpcClient<Channel>,
+    hash: String,
+    include_transactions: bool
+) -> Result<Option<KaspadResponse>, tonic::Status> {
+
+    let outbound = async_stream::stream! {
+        let request = KaspadRequest {
+            payload:
+                Some(kaspad_request::Payload::GetBlockRequest(
+                    GetBlockRequestMessage { hash: hash, include_transactions: include_transactions }
+                ))
+            };
+        yield request;
+    };
+
+    let response = client.message_stream(Request::new(outbound)).await?;
+    let mut inbound = response.into_inner();
+
+    let result = inbound.message().await;
+    result
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = RpcClient::connect("http://[::1]:10000").await?;
 
-    println!("*** SIMPLE RPC ***");
-    let response = client
-        .message_stream(Request::new(KaspadRequest {
-            payload:
-                Some(kaspad_request::Payload::GetBlockRequest(
-                    GetBlockRequestMessage { hash: String::from("A"), include_transactions: false }
-                ))
-            }))
-        .await?;
+    println!("*** ONE ROUND-TRIP RPC ***");
+    let response = run_get_block(&mut client, String::from("A"), false).await?;
     println!("RESPONSE = {:#?}", response);
 
     Ok(())
--- src/server.rs
+++ src/server.rs
@@ -7,47 +7,61 @@ use poly_rpc_prototype::protowire::{
 };
 
 // use std::collections::HashMap;
-// use std::pin::Pin;
+use std::pin::Pin;
 // use std::sync::Arc;
 // use std::time::Instant;
 
-// use futures::{Stream, StreamExt};
+use futures::{Stream, StreamExt};
 // use tokio::sync::mpsc;
 // use tokio_stream::wrappers::ReceiverStream;
-use tonic::transport::Server;
-use tonic::Response;
+use tonic::{
+    transport::Server,
+    Request, Response
+};
 
 #[derive(Debug)]
 struct RpcService;
 
 #[tonic::async_trait]
 impl Rpc for RpcService {
+
+    type MessageStreamStream = Pin<Box<dyn Stream<Item = Result<KaspadResponse, tonic::Status>> + Send + 'static>>;
+
     async fn message_stream(
         &self,
-        request: tonic::Request<KaspadRequest>,
-    ) -> Result<tonic::Response<KaspadResponse>, tonic::Status> {
-        println!("GetBlock = {:?}", request);
+        request: Request<tonic::Streaming<KaspadRequest>>,
+    ) -> Result<Response<Self::MessageStreamStream>, tonic::Status> {
+        println!("MessageStream");
 
-        let request = request.into_inner();
-        let payload = match request.payload {
-            Some(kaspad_request::Payload::NotifyBlockAddedRequest(_notify_block_added_request_message)) => {
-                Some(kaspad_response::Payload::NotifyBlockAddedResponse(NotifyBlockAddedResponseMessage {
-                    error: None
-                }))
-            },
-            Some(kaspad_request::Payload::GetBlockRequest(_get_block_request_message)) => {
-                Some(kaspad_response::Payload::GetBlockResponse(GetBlockResponseMessage {
-                    block: Some(create_dummy_rpc_block()),
-                    error: None,
-                }))
-            },
-            None => None,
-        };
-        let response = KaspadResponse {
-            payload: payload
+        let mut stream = request.into_inner();
+
+        let output = async_stream::try_stream! {
+            while let Some(request) = stream.next().await {
+                let request = request?;
+                
+                let payload = match request.payload {
+                    Some(kaspad_request::Payload::NotifyBlockAddedRequest(_notify_block_added_request_message)) => {
+                        Some(kaspad_response::Payload::NotifyBlockAddedResponse(NotifyBlockAddedResponseMessage {
+                            error: None
+                        }))
+                    },
+                    Some(kaspad_request::Payload::GetBlockRequest(_get_block_request_message)) => {
+                        Some(kaspad_response::Payload::GetBlockResponse(GetBlockResponseMessage {
+                            block: Some(create_dummy_rpc_block()),
+                            error: None,
+                        }))
+                    },
+                    None => None,
+                };
+                let response = KaspadResponse {
+                    payload: payload
+                };
+
+                yield response;
+            }
         };
 
-        return Ok(Response::new(response));
+        Ok(Response::new(Box::pin(output) as Self::MessageStreamStream))
     }
 }
 
